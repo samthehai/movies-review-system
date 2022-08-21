c665933f48a54928fc4a4b9b615c1a48bc2504a0
Hai Sam <samthehai@gmail.com>
2022-08-21 10:37:46 +0900

    Add some tools to tool.go

--- tools.go
+++ tools.go
@@ -3,5 +3,8 @@
 package tools
 
 import (
+	_ "github.com/golang/mock/mockgen"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/rubenv/sql-migrate/sql-migrate"
+	_ "golang.org/x/tools/cmd/goimports"
 )
